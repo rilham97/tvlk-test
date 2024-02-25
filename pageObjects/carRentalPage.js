f864d57fca7bf5fb11fc44f873d5752bb3513744
Ilham Ramadhan <rama@Ramas-MBP.lan>
2024-02-25 21:23:48 +0700

    Complete test cases, adjust ReadMe, Add screenshot feature

--- pageObjects/carRentalPage.js
+++ pageObjects/carRentalPage.js
@@ -1,5 +1,6 @@
 // pageObjects/carRentalPage.js
 import { By, until } from "selenium-webdriver";
+import { Key } from "selenium-webdriver";
 import elements from "../elementIds/carRentalElements.js";
 
 class CarRentalPage {
@@ -9,38 +10,59 @@ class CarRentalPage {
 
   // Utility function to wait for an element to be present
   async waitForElement(locator, timeout = 10000) {
-    await this.driver.wait(until.elementLocated(locator), timeout);
-    return this.driver.findElement(locator);
+    try {
+      // Wait for the element to be located
+      const element = await this.driver.wait(
+        until.elementLocated(locator),
+        timeout
+      );
+      // Wait for the element to be visible
+      await this.driver.wait(until.elementIsVisible(element), timeout);
+      // Wait for the element to be clickable (visible and enabled)
+      await this.driver.wait(until.elementIsEnabled(element), timeout);
+      return element;
+    } catch (error) {
+      if (error.name === "TimeoutError") {
+        console.error(
+          `TimeoutError: Waiting for element ${locator} timed out after ${timeout}ms`
+        );
+      } else {
+        console.error(`Error waiting for element ${locator}: ${error}`);
+      }
+      throw error; // Re-throw the error after logging it
+    }
   }
 
   async selectCarsProduct() {
     const carRentalMenu = await this.waitForElement(
-      By.css('[data-id="IcProductDuotoneCarRentalFill"]')
+      By.css(elements.carRentalMenu)
     );
     await carRentalMenu.click();
   }
 
   async selectWithoutDriverTab() {
-    const carRentalMenu = await this.waitForElement(
-      By.css('[data-id="IcTransportPickUpDriver"]')
+    const withoutDriver = await this.waitForElement(
+      By.css(elements.withoutDriver)
     );
-    await carRentalMenu.click();
+    await withoutDriver.click();
   }
 
   async selectPickUpLocation(location) {
     const locationInput = await this.waitForElement(
-      By.css('[data-testid="rental-search-form-location-input"]')
+      By.css(elements.locationInput)
     );
+
     await locationInput.sendKeys(location);
+    await this.driver.sleep(1000);
     const resultData = await this.waitForElement(
-      By.css('[data-testid="rental-search-form-location-item"]')
+      By.css(elements.resultLocation)
     );
     await resultData.click();
   }
 
   async selectPickUpDateAndTime(date, time) {
     const pickUpDate = await this.waitForElement(
-      By.css('[data-testid="rental-search-form-date-input-start"]')
+      By.css(elements.datePickerPickup)
     );
     await pickUpDate.click();
 
@@ -50,94 +72,288 @@ class CarRentalPage {
     await dateElement.click();
 
     const pickUpTime = await this.waitForElement(
-        By.css('[data-testid="rental-search-form-time-input-start"]')
-      );
-      await pickUpTime.click();
+      By.css(elements.timePickerPickup)
+    );
 
+    await pickUpTime.click();
+    await this.driver.sleep(1000);
     const [hours, minutes] = time.split(":").map((num) => parseInt(num, 10));
 
+    // Find the hour element by navigating from the "Hour" label
     const hourElement = await this.waitForElement(
-      By.css(`div[data-testid="hour-${hours}"]`)
+      By.xpath(
+        `//div[contains(text(), 'Hour')]/following-sibling::div//div[@dir='auto' and contains(text(), '${hours}')][1]`
+      )
     );
     await hourElement.click();
-
+    // Find the minute element by navigating from the "Minute" label
     const minuteElement = await this.waitForElement(
-      By.css(`div[data-testid="minute-${minutes}"]`)
+      By.xpath(
+        `//div[contains(text(), 'Minute')]/following-sibling::div//div[@dir='auto' and contains(text(), '${minutes}')][1]`
+      )
     );
     await minuteElement.click();
+
+    const doneButton = await this.waitForElement(
+      By.xpath(elements.timePickerDoneButton)
+    );
+    await doneButton.click();
   }
 
   async selectDropOffDateAndTime(date, time) {
-
     const dropOffDate = await this.waitForElement(
-      By.css('[data-testid="rental-search-form-date-input-end"]')
+      By.css(elements.datePickerDrop)
     );
     await dropOffDate.click();
-
-    const dateElement = await this.waitForElement(
-      By.css(`[data-testid="date-cell-${date}"] > div`)
+    const parentElement = await this.waitForElement(
+      By.xpath(elements.containerDatePicker)
+    );
+    const dateElement = await parentElement.findElement(
+      By.css(`[data-testid="date-cell-${date}"]`)
     );
     await dateElement.click();
 
     const dropOffTime = await this.waitForElement(
-        By.css('[data-testid="rental-search-form-time-input-end"]')
-      );
-      await dropOffTime.click();
+      By.css(elements.timePickerDrop)
+    );
+    await dropOffTime.click();
+    await this.driver.sleep(1000);
 
     const [hours, minutes] = time.split(":").map((num) => parseInt(num, 10));
-
+    // Find the hour element by navigating from the "Hour" label
     const hourElement = await this.waitForElement(
-      By.css(`div[data-testid="hour-${hours}"]`)
+      By.xpath(
+        `//div[contains(text(), 'Hour')]/following-sibling::div//div[@dir='auto' and contains(text(), '${hours}')][1]`
+      )
     );
     await hourElement.click();
-
+    // Find the minute element by navigating from the "Minute" label
     const minuteElement = await this.waitForElement(
-      By.css(`div[data-testid="minute-${minutes}"]`)
+      By.xpath(
+        `//div[contains(text(), 'Minute')]/following-sibling::div//div[@dir='auto' and contains(text(), '${minutes}')][1]`
+      )
     );
     await minuteElement.click();
+
+    const doneButton = await this.waitForElement(
+      By.xpath(elements.timePickerDoneButton)
+    );
+    await doneButton.click();
   }
 
   async clickSearchCar() {
     const searchButton = await this.waitForElement(
-      By.css('[data-testid="rental-search-form-cta"]')
+      By.css(elements.searchCarButton)
     );
     await searchButton.click();
   }
 
   async selectCar() {
-    const potentialButtons = await this.driver.findElements(By.xpath('//div[@role="button"]'));
-  
-    for (const button of potentialButtons) {
-      const text = await button.getText(); 
-      if (text.trim() === "Continue") {
-        await button.click(); 
-        break; // Exit the loop after clicking the first "Continue" button
-      }
-    }
+    let continueButton = await this.waitForElement(
+      By.xpath(elements.selectCarFirstOption)
+    );
+    // Click the "Continue" button
+    await continueButton.click();
   }
 
   async selectCarProvider() {
-    const potentialButtons = await this.driver.findElements(By.xpath('//div[@role="button"]'));
-  
-    for (const button of potentialButtons) {
-      const text = await button.getText(); 
-      if (text.trim() === "Continue") {
-        await button.click(); 
-        break; // Exit the loop after clicking the first "Continue" button
-      }
-    }
+    let continueButton = await this.waitForElement(
+      By.xpath(elements.selectCarFirstOption)
+    );
+    // Click the "Continue" button
+    await continueButton.click();
   }
 
   async selectPickUpLocationInRentalOffice() {
-    const rentalPickupLocation = await this.waitForElement(
-      By.id('RENTAL_PICKUP_LOCATION')
+    const rentalPickupLocationContainer = await this.waitForElement(
+      By.id(elements.rentalPickupContainer)
     );
-  
-    const rentalOfficeButton = await rentalPickupLocation.findElement(
-      By.xpath(".//div[contains(text(), 'Rental Office')]")
+
+    const rentalOfficeButton = await rentalPickupLocationContainer.findElement(
+      By.xpath(elements.rentalPickupOfficeButton)
     );
-  
+
+    await this.driver.executeScript(
+      "arguments[0].scrollIntoView(true);",
+      rentalOfficeButton
+    );
+
     await rentalOfficeButton.click();
+  }
+
+  async selectLocationPickup() {
+    const rentalOfficeDropDown = await this.waitForElement(
+      By.xpath(elements.rentalOfficeDropdown)
+    );
+
+    await rentalOfficeDropDown.click();
+    const officeAddressList = await this.waitForElement(
+      By.xpath(elements.officeAddressData)
+    );
+
+    await officeAddressList.click();
+
+    // await this.driver.sleep(5000);
+  }
+
+  async selectDropOffLocationInOtherLocation() {
+    const rentalDropOffLocationContainer = await this.waitForElement(
+      By.id(elements.rentalDropOffContainer)
+    );
+
+    const otherLocationsRadioButton = await rentalDropOffLocationContainer.findElement(
+        By.xpath(elements.otherLocationsRadio)
+      );
+
+    await this.driver.executeScript(
+      "arguments[0].scrollIntoView(true);",
+      otherLocationsRadioButton
+    );
+
+    await otherLocationsRadioButton.click();
+  }
+
+  async inputPickUpDropOffNotes(notes) {
+    const notesTextarea = await this.waitForElement(
+      By.css(elements.textAreaNotePickup)
+    );
+    await notesTextarea.sendKeys(notes);
+  }
+
+  async clickBookNow() {
+    const bookNowButton = await this.waitForElement(
+      By.xpath(elements.bookNowButton)
+    );
+    await bookNowButton.click();
+
+    await this.driver.sleep(10000);
+  }
+
+  async fillContactDetails(contactDetails) {
+    const fullNameForm = await this.waitForElement(
+      By.xpath(elements.fullNameFormContact)
+    );
+    await fullNameForm.sendKeys(contactDetails.name);
+
+    const numberForm = await this.waitForElement(
+      By.xpath(elements.phoneFormContact)
+    );
+    await numberForm.sendKeys(contactDetails.no);
+
+    const emailForm = await this.waitForElement(
+      By.xpath(elements.emailFormContact)
+    );
+    await emailForm.sendKeys(contactDetails.email);
+  }
+
+  async fillDriverDetails(driverDetails) {
+    const parentElement = await this.waitForElement(By.id(elements.driverFormContainer));
+
+    const dropdown = await parentElement.findElement(
+      By.xpath(elements.dropdownTitle)
+    );
+
+    await this.driver.executeScript(
+      "arguments[0].scrollIntoView(true);",
+      dropdown
+    );
+
+    await dropdown.click();
+
+    const optionMr = await this.waitForElement(
+      By.xpath(`//option[@value='${driverDetails.driverTitle}']`)
+    );
+    await optionMr.click();
+    await dropdown.sendKeys(Key.ESCAPE);
+
+    const fullNameForm = await parentElement.findElement(
+      By.xpath(elements.fullNameFormDriver)
+    );
+    await fullNameForm.sendKeys(driverDetails.driverName);
+
+    const numberForm = await parentElement.findElement(
+      By.xpath(elements.phoneFormDriver)
+    );
+    await numberForm.sendKeys(driverDetails.driverNo);
+  }
+
+  async clickContinueAfterDetails() {
+    const continueButton = await this.waitForElement(
+      By.xpath(elements.continueButtonRentalDetail)
+    );
+
+    await continueButton.click();
+  }
+
+  async addSpecialRequest(notes) {
+    const notesTextarea = await this.waitForElement(
+      By.css(elements.specialRequestTextArea)
+    );
+    await this.driver.executeScript(
+      "arguments[0].scrollIntoView(true);",
+      notesTextarea
+    );
+
+    await notesTextarea.sendKeys(notes);
+  }
+
+  async checkRentalRequirements() {
+    const buttonRequirements = await this.waitForElement(
+      By.css(elements.checkRequirementButton)
+    );
+    await buttonRequirements.click();
+
+    await this.driver.sleep(2000);
+
+    const checkAll = await this.waitForElement(
+      By.xpath(elements.checkAll)
+    );
+    await checkAll.click();
+
+    const saveButton = await this.waitForElement(
+      By.xpath(elements.saveButton)
+    );
+    await saveButton.click();
+  }
+
+  async clickContinueAfterRequirements() {
+    const continuePaymentButton = await this.waitForElement(
+      By.xpath(elements.continuePayment)
+    );
+
+    await this.driver.executeScript(
+      "arguments[0].scrollIntoView(true);",
+      continuePaymentButton
+    );
+
+    await continuePaymentButton.click();
+
+    await this.driver.sleep(3000);
+
+    const continueButton = await this.waitForElement(
+      By.css(elements.continueConfirmation)
+    );
+    await continueButton.click();
+  }
+
+  async selectPaymentMethodAndProceed() {
+    const transferOption = await this.waitForElement(
+      By.css(elements.transferMenu)
+    );
+    await transferOption.click();
+
+    const proceedTransfer = await this.waitForElement(
+      By.css(elements.payWithTransferButton)
+    );
+    await proceedTransfer.click();
+
+    await this.waitForElement(
+      By.css(elements.paymentInstructionLabel)
+    );
+
+    await this.waitForElement(
+      By.css(elements.transferAmountLabel)
+    );
   }
 }
 
--- test/carRentalTest.js
+++ test/carRentalTest.js
@@ -2,7 +2,9 @@ import { Builder, By } from 'selenium-webdriver';
 import CarRentalPage from '../pageObjects/carRentalPage.js';
 import config from '../config.json' assert { type: 'json' };
 import { describe, it, before, after } from 'mocha';
-import { expect } from 'chai';
+import fs from 'fs-extra';
+
+
 
 describe('Car Rental Scenario', function() {
     this.timeout(config.timeout);
@@ -10,89 +12,108 @@ describe('Car Rental Scenario', function() {
     let carRentalPage;
 
     before(async function() {
+        await fs.emptyDir('output');
+        await fs.mkdirp('output');
         driver = await new Builder().forBrowser('chrome').build();
+        await driver.manage().window().maximize();
         carRentalPage = new CarRentalPage(driver);
         await driver.get(config.baseUrl);
     });
 
-    it('Select Cars Product', async function() {
+    it('1. Select Cars Product', async function() {
         await carRentalPage.selectCarsProduct();
     });
 
-    it('Select tab Without Driver', async function() {
+    it('2. Select tab Without Driver', async function() {
         await carRentalPage.selectWithoutDriverTab();
     });
 
-    it('Select Pick-up Location', async function() {
+    it('3. Select Pick-up Location', async function() {
         await carRentalPage.selectPickUpLocation('Gambir Station');
     });
 
-    it('Select Pick-up Date & Time', async function() {
-        await carRentalPage.selectPickUpDateAndTime('27-2-2024', '09:00');
+    it('4. Select Pick-up Date & Time', async function() {
+        await carRentalPage.selectPickUpDateAndTime('28-2-2024', '10:30');
     });
 
-    it('Select Drop-off Date & Time', async function() {
-        await carRentalPage.selectDropOffDateAndTime('29-2-2024', '11:00');
+    it('5. Select Drop-off Date & Time', async function() {
+        await carRentalPage.selectDropOffDateAndTime('1-3-2024', '11:00');
     });
 
-    it('Click button Search Car', async function() {
+    it('6. Click button Search Car', async function() {
         await carRentalPage.clickSearchCar();
     });
 
-    it('Select Car', async function() {
+    it('7. Select Car', async function() {
         await carRentalPage.selectCar();
     });
 
-    it('Select Car Provider', async function() {
+    it('8. Select Car Provider', async function() {
         await carRentalPage.selectCarProvider();
     });
 
-    // it('Click button Continue in Product Detail', async function() {
-    //     await carRentalPage.clickContinueInProductDetail();
-    // });
-
-    it('Select Pick-up Location in “Rental Office”', async function() {
+    it('9. Select Pick-up Location in “Rental Office”', async function() {
         await carRentalPage.selectPickUpLocationInRentalOffice();
     });
 
-    it('Select Drop-off Location in “Other Location”', async function() {
+    it('10. Select Location Pickup', async function() {
+        await carRentalPage.selectLocationPickup();
+    });
+    
+
+    it('11. Select Drop-off Location in “Other Location”', async function() {
         await carRentalPage.selectDropOffLocationInOtherLocation();
     });
 
-    it('Input Pick-up/Drop-off notes (optional)', async function() {
-        await carRentalPage.inputPickUpDropOffNotes();
+    it('12. Input Pick-up or Drop-off notes (optional)', async function() {
+        await carRentalPage.inputPickUpDropOffNotes('Good Morning Traveloka');
     });
 
-    it('Click button Book Now', async function() {
+    it('13. Click button Book Now', async function() {
         await carRentalPage.clickBookNow();
     });
 
-    it('Fill Contact Details', async function() {
-        await carRentalPage.fillContactDetails();
+    it('14. Fill Contact Details', async function() {
+        await carRentalPage.fillContactDetails({
+            name: 'Ilham Ramadhan',
+            no: '88888888',
+            email: 'test@example.com'
+        });
     });
 
-    it('Fill Driver Details', async function() {
-        await carRentalPage.fillDriverDetails();
+    it('15. Fill Driver Details', async function() {
+        await carRentalPage.fillDriverDetails({
+            driverTitle: 'MR',
+            driverName: 'Ilham Ramadhan',
+            driverNo: '999999999',
+        });
     });
 
-    it('Click Continue after filling details', async function() {
+    it('16. Click Continue after filling details', async function() {
         await carRentalPage.clickContinueAfterDetails();
     });
 
-    it('Add a special request (optional)', async function() {
-        await carRentalPage.addSpecialRequest();
+    it('17. Add a special request (optional)', async function() {
+        await carRentalPage.addSpecialRequest('Pengen mobilnya yang bagus ya');
     });
 
-    it('Check all rental requirements', async function() {
+    it('18. Check all rental requirements', async function() {
         await carRentalPage.checkRentalRequirements();
     });
 
-    it('Click Continue after checking requirements', async function() {
+    it('19. Click Continue after checking requirements', async function() {
         await carRentalPage.clickContinueAfterRequirements();
     });
 
-    it('Select payment method and proceed to payment', async function() {
+    it('20. Select payment method and proceed to payment', async function() {
         await carRentalPage.selectPaymentMethodAndProceed();
+    });
+
+    afterEach(async function() {
+        const projectDir = process.cwd();
+        const screenshot = await driver.takeScreenshot();
+        const filePath = `${projectDir}/output/screenshot-${this.currentTest.title}.png`
+        fs.writeFileSync(filePath, screenshot, 'base64');
     });
 
     after(async function() {
